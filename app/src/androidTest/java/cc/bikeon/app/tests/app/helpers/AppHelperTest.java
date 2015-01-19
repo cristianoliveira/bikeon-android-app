400b05af20e445faac6704b01b14df65abb4b60f
CristianOliveiraDaRosa <contato@cristianoliveira.com.br>
2015-01-19 18:45:44 -0200

    AppHelper Test Case

---
+++ app/src/androidTest/java/cc/bikeon/app/tests/app/helpers/AppHelperTest.java
@@ -0,0 +1,16 @@
+package cc.bikeon.app.tests.app.helpers;
+
+import android.app.Application;
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+/**
+ * Created by cristian.rosa on 1/19/2015.
+ */
+public class AppHelperTest extends AndroidTestCase {
+    @SmallTest
+    public void testGetApplicationInstance()
+    {
+        Application app = Apps
+    }
+}
---
+++ app/src/main/java/cc/bikeon/app/BikeOnApplication.java
@@ -0,0 +1,7 @@
+package cc.bikeon.app;
+
+/**
+ * Created by cristian.rosa on 1/19/2015.
+ */
+public class BikeOnApplication {
+}
