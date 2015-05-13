0d4079f070f216ec70a480e02b9f6fdf074dac2e
Cristian Oliveira <contato@cristianoliveira.com.br>
2015-05-13 00:50:00 -0300

    [New] Weather Rest API interface

---
+++ app/src/androidTest/java/cc/bikeon/app/services/RestClientTest.java
@@ -0,0 +1,32 @@
+package cc.bikeon.app.services;
+
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import cc.bikeon.app.services.weather.WeatherService;
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by cristianoliveira on 12/05/15.
+ */
+public class RestClientTest extends AndroidTestCase {
+
+    @SmallTest
+    public void testWhenRequestARestApiItShouldReturnService()
+    {
+        // given
+        IRestProvider provider = new StubProvider();
+        Class serviceClass = WeatherService.class;
+        RestClient restClient = new RestClient(provider);
+
+        // when
+        WeatherService service = (WeatherService) restClient.getService(serviceClass);
+
+        // then
+        assertNotNull(service);
+    }
+
+}
---
+++ app/src/main/java/cc/bikeon/app/services/IRestProvider.java
@@ -0,0 +1,8 @@
+package cc.bikeon.app.services;
+
+/**
+ * Created by cristianoliveira on 12/05/15.
+ */
+public interface IRestProvider {
+    public String getBaseUrl();
+}
