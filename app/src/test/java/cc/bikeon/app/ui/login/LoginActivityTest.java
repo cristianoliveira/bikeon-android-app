0d7c52af38a9a02ff33d6e1228296883849d3390
Cristian Oliveira da Rosa <contato@cristianoliveira.com.br>
2015-08-23 23:35:31 -0300

    Clean values.

---
+++ app/src/test/java/cc/bikeon/app/ui/login/LoginActivityTest.java
@@ -0,0 +1,24 @@
+package cc.bikeon.app.ui.login;
+
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import cc.bikeon.app.BaseRoboletricTestRunner;
+import cc.bikeon.app.BuildConfig;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests for {@link LoginActivity}
+ * Created by cristianoliveira on 23/08/15.
+ */
+@RunWith(BaseRoboletricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class LoginActivityTest {
+
+    public void itShouldShowLoginOptionsWhenUserHasNoSession() {
+
+
+    }
+
+}
